26bea609a4fa0dc8472a39a4d6110f3ea131b105
luiz0809 <luiz.souza@bandtec.com.br>
2022-03-01 23:50:38 -0300

    Banco de Dados H2, test profile e JPA

--- src/main/java/com/example/treinospring/entidades/Cliente.java
+++ src/main/java/com/example/treinospring/entidades/Cliente.java
@@ -1,11 +1,21 @@
 package com.example.treinospring.entidades;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Cliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String CPF;
